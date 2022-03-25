4292950e21402bb63f15df15bd6c0d4b1dbfb083
김재훈 <jhkim@mobileleader.com>
2022-03-25 15:39:18 +0900

    테스트 코드 작성

--- app/src/test/java/com/jhkwim/news/NewsSearchViewModelTest.kt
+++ app/src/test/java/com/jhkwim/news/NewsSearchViewModelTest.kt
@@ -1,73 +1,112 @@
 package com.jhkwim.news
 
 import android.net.Uri
+import android.text.Html
+import android.text.Spanned
 import com.jhkwim.news.api.News
 import com.jhkwim.news.api.ResultNews
 import com.jhkwim.news.repository.NewsRepository
 import com.jhkwim.news.search.NewsSearchViewModel
-import com.jhkwim.news.utils.LiveDataUtils.getOrAwaitValue
-import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
-import io.mockk.*
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.verify
 import io.reactivex.rxjava3.core.Observable
 
-class NewsSearchViewModelTest : StringSpec({
+class NewsSearchViewModelTest : BehaviorSpec({
 
     lateinit var mockNewsRepository: NewsRepository
     lateinit var viewModel: NewsSearchViewModel
-
-    val dummySearchString = "네이버"
-    val dummyNews = News("title", "link", "description", "pubDate")
-    val dummyList = listOf(dummyNews)
 
     beforeSpec {
         mockNewsRepository = mockk()
         viewModel = NewsSearchViewModel(mockNewsRepository)
     }
 
-    "검색버튼 활성화/비활성화 테스트" {
-        viewModel.searchText.value = null
+    Given("검색어가") {
 
-        viewModel.isSearchButtonEnabled() shouldBe false
+        When("입력이 되어있으면") {
+            viewModel.searchTextChanged("test")
+            then("입력버튼이 활성화 된다.") {
+                viewModel.isEnabledSearchButton.value shouldBe true
+            }
+        }
 
-        viewModel.searchText.value = dummySearchString
+        When("공백이면") {
+            viewModel.searchTextChanged("")
+            then("입력버튼이 비활성화된다.") {
+                viewModel.isEnabledSearchButton.value shouldBe false
+            }
+        }
 
-        viewModel.isSearchButtonEnabled() shouldBe true
+        When("띄어쓰기면") {
+            viewModel.searchTextChanged(" ")
+            then("입력버튼이 비활성화된다.") {
+                viewModel.isEnabledSearchButton.value shouldBe false
+            }
+        }
     }
 
-//    "검색버튼 테스트" {
-//
-//
-//        viewModel.onSearchButtonClicked(mockk())
-//
-//
-//    }
-//
-//    "뉴스 검색 후 searchedNews에 결과값이 반영 되는가" {
-//        val searchString = "네이버"
-//
-//        viewModel.searchText.value = searchString
-//
-//        val expected = Observable.just(ResultNews(dummyList))
-//
-//        every { mockNewsRepository.getNews(searchString) } returns expected
-//
-////        viewModel.getNews()
-//
-//        verify(exactly = 1) {
-//            mockNewsRepository.getNews(searchString)
-//        }
-//
-//        viewModel.searchedNews.value shouldBe dummyList
-//    }
+    Given("검색어를 입력 후") {
+        val dummySearchString = "검색어"
+        viewModel.searchTextChanged(dummySearchString)
 
-    "링크" {
-        mockkStatic(Uri::class)
-        val uriMock = mockk<Uri>()
-        every { Uri.parse(dummyNews.link) } returns uriMock
+        When("검색 결과를 가져오는 중 에러가 발생한 경우") {
+            val throwable = Throwable("에러 발생")
+            val expected = Observable.error<ResultNews>(throwable)
+            every { mockNewsRepository.searchNews(dummySearchString) } returns expected
 
-        viewModel.toUri(0)
-        viewModel.uri.value shouldBe uriMock
+            viewModel.onSearchButtonClicked(dummySearchString)
 
+            Then("에러를 전달한다.") {
+                viewModel.error.value shouldBe Error(throwable)
+            }
+        }
+
+        When("검색이 완료된 경우") {
+            val dummyNews = News("검색어", "링크", "내용", "Wed, 23 Mar 2022 13:18:00 +0900")
+            val dummyList = listOf(dummyNews)
+
+            val expected = Observable.just(ResultNews(dummyList))
+            every { mockNewsRepository.searchNews(dummySearchString) } returns expected
+
+            viewModel.onSearchButtonClicked(dummySearchString)
+
+            Then("뉴스 검색 결과를 전달한다.") {
+                verify {
+                    mockNewsRepository.searchNews(dummySearchString)
+                }
+                viewModel.searchedNews.value shouldBe dummyList
+            }
+
+            Then("뉴스 검색 결과의 타이틀을 가져온다.") {
+                mockkStatic(Html::class)
+                val spannedMock = mockk<Spanned>()
+                every {
+                    Html.fromHtml(
+                        dummyNews.title,
+                        Html.FROM_HTML_MODE_LEGACY
+                    )
+                } returns spannedMock
+
+                viewModel.getTile(0) shouldBe spannedMock
+            }
+
+            Then("뉴스 검색 결과의 게시 날짜를 가져온다.") {
+                val date = viewModel.getDate(0)
+                date shouldBe viewModel.formatDate(dummyNews.pubDate)
+            }
+
+            Then("뉴스 검색 결과의 링크주소를 가져온다.") {
+                mockkStatic(Uri::class)
+                val uriMock = mockk<Uri>()
+                every { Uri.parse(dummyNews.link) } returns uriMock
+
+                viewModel.toUri(0)
+                viewModel.uri.value shouldBe uriMock
+            }
+        }
     }
 })
